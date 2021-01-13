17555253241d990e8bc14a4eb3b03ba4ed70d756
Andrii Lebukhorskyi <andrey.lebuhorskyy@gmail.com>
2021-01-13 16:13:09 +0200

    Admin panel for users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::Base
+  layout :choose_layout
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:username])
+  end
+
+  def choose_layout
+    current_user ? 'account' : 'application'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'home#index'
+  namespace :account do
+    namespace :admin do
+      resources :users, except: %i[show]
+    end
+  end
 end
